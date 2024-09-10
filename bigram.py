eeae24a3a7db7f367b7443849b1e9dc6e4ba44a3
glitchyfuzzy <saran.wrap.2500@gmail.com>
2024-09-10 07:41:17 -0400

    skibidi dop dop dop yes yes yes

--- bigram.py
+++ bigram.py
@@ -14,7 +14,7 @@ eval_iters = 200
 
 torch.manual_seed(1337)
 
-# wget https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt
+# wget from input.txt
 with open('input.txt', 'r', encoding='utf-8') as f:
     text = f.read()
 
